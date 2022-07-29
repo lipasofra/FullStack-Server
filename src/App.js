8407c7c93fe9089906412f9b97f7d402dad41dd2
Lina Soler <lipasofra.ph@gmail.com>
2022-07-28 19:02:45 -0500

    reorganizing the code

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import './App.css';
 
-const App = (props) => {
-  const { notes } = props
+const Note = ({ note }) => {
+  return (
+    <li>{note.content}</li>
+  )
+}
+
+
+const App = ({ notes }) => {
 
   return (
     <div>
       <h1>Notes</h1>
       <ul>
         {notes.map(note => 
-          <li key={note.id}>
-            {note.content}
-          </li>
+          <Note key={note.id} note={note}/>
         )}
       </ul>
     </div>
