76882c6397b1527ac705a6361514eceab838cc3d
Lina Soler <lipasofra.ph@gmail.com>
2022-08-01 19:38:01 -0500

    controlled component input in form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import Note from './components/Note'
+import { useState } from 'react'
 
-const App = ({ notes }) => {
+const App = ( props ) => {
+
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('a new note...')
+
+  const addNote = (event) => {
+    event.preventDefault()
+    console.log("button clicked", event.target)
+  }
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
 
   return (
     <div>
@@ -10,6 +24,10 @@ const App = ({ notes }) => {
           <Note key={note.id} note={note}/>
         )}
       </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange}/>
+        <button type='submit'>save</button>
+      </form>
     </div>
   )
 }
